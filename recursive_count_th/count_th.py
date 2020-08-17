1a9866f975eda737295c1dc233bd4bd4cc6763db
Westley Feller <feller.westley@gmail.com>
2020-08-16 19:14:35 -0700

    Counting 'th' complete

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,23 @@
-'''
+"""
 Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
-'''
-def count_th(word):
-    
-    # TBC
-    
-    pass
+"""
+
+
+def count_th(word: str) -> int:
+    if len(word) <= 1:
+        if hasattr(count_th, "count"):
+            count = count_th.count
+            count_th.count = 0
+            return count
+
+        return 0
+
+    if word[:2] == "th":
+        if hasattr(count_th, "count"):
+            count_th.count += 1
+        else:
+            count_th.count = 1
+
+    return count_th(word[1:])
